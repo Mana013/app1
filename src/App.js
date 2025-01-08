e65c13351faf8cbed73f873571e98e8ce763f146
Mana013 <manaza17801524@gmail.com>
2025-01-08 14:59:16 +0700

    add calculator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import { Content, Footer, Header } from './func-components';
-import  Calendar from './class-components';
+import {Calculator2} from './calculator'
 
 
 function App() {
-  return (
-   <>
-   <Header/>
-   <p><center><Calendar/></center></p>
-   <Content/>
-   <Footer/>
-   </>
-    
-  );
+  return <Calculator2/>
 }
 
 export default App;
