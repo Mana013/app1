7fd85e79ffc4022f53119bb00b1056606425ccdd
Mana013 <manaza17801524@gmail.com>
2025-03-29 21:01:14 +0700

    สอบปฏิบัติ_ECT_เปลี่ยนเป็นการบ้าน

--- karaoke.js
+++ karaoke.js
@@ -3,6 +3,7 @@ function calculate() {
     let rate = 120;
     let hours = 0;
   
+    // ตรวจสอบว่าผู้ใช้เลือกอะไร
     if (rateElements[1].checked) {
       rate = 80;
       hours = parseInt(document.getElementById("hours2").value);
@@ -11,15 +12,17 @@ function calculate() {
       hours = parseInt(document.getElementById("hours1").value);
     }
   
+    // ตรวจสอบว่ากรอกจำนวนชั่วโมงหรือไม่
     if (isNaN(hours) || hours === 0) {
       alert("กรุณากรอกจำนวนชั่วโมง");
       return;
     }
   
+    // คำนวณราคา
     const total = rate * hours;
     document.getElementById("total").value = total;
   
-    // ทำ input เป็น readonly
+    // ทำให้ input เป็น readonly
     document.getElementById("hours1").readOnly = true;
     document.getElementById("hours2").readOnly = true;
   }
